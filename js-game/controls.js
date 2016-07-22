70b9dfde9a5e14cf04aaa864014c33d3641ba6e3
Dariel360 <Dariel360@yahoo.com>
2016-07-22 09:48:25 -0400

    Add files via upload

--- js-game/controls.js
+++ js-game/controls.js
@@ -17,3 +17,11 @@ $(document). keyup(function(e){
   if(e.keyCode == 39) player.velX = 0;
   if(e.keyCode == 40) player.velY = 0;
 });
+$(document).on("touchstart", function(e){
+  var touchX=e.touches[0].clientX;
+  var touchY = e.touches [0].clientY;
+  if (touchX < player.x) player.velX = -10;
+  if (touchX > player.x) player.velX = 10;
+  if (touchY < player.y) player.velY = -10;
+  if (touchY > player.y) player.velY = 10;
+});

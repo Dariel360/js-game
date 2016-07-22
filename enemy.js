4d05f0683b27d27a56dcfb2bb699b9dfce1aa40e
Dariel360 <Dariel360@yahoo.com>
2016-07-22 09:55:16 -0400

    Add files via upload

---
+++ enemy.js
@@ -0,0 +1,31 @@
+var enemy = {
+  x:100,
+  y:600,
+velX : 0,
+velY : 0,
+sprite: [document.getElementById("enemy")],
+update: function(){
+  var a= (this.x-player.x)*(this.x-player.x);
+  var b = (this.y-player.y)*(this.y-player.y);
+  var dist= Math.sqrt(a+b);
+  if (dist< 10){
+    player.dead = true;
+  }
+  if (this.x < player .x){
+    this.x = this.y + 3;
+  }
+  if (this.x > player.x){
+    this.x = this.x -3;
+  }
+  if(this.y < player.y){
+    this.y = this.y + 3;
+  }
+  if(this.y > player.y ){
+    this.y = this.y -3;
+  }
+},
+render : function(){
+  ctx.drawImage(this.sprite[0], this.x, this.y);
+  restoreCtx(ctx);
+}
+};
